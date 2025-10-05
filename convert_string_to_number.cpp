c56ba101e5106ecab792f096939f2cccaa665284
Antony Merle <antony.merle@gmail.com>
2025-10-05 16:12:02 +0200

    convert_string_to_number

---
+++ convert_string_to_number.cpp
@@ -0,0 +1,41 @@
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+void convert_string_to_number()
+{
+    std::string s{"-100"};
+    int i = std::stoi(s);
+    std::cout << i << "\n";
+
+    std::string s2{"    -2025     "};
+    long l = std::stol(s2);
+    std::cout << l << "\n";
+
+    std::string s3{"64600 Anglet"};
+    unsigned long ul = std::stoul(s3);
+    std::cout << ul << "\n";
+
+    std::string s4{"3.1415926535"};
+    float f = std::stof(s4);
+    std::cout << f << "\n";
+
+    std::string s5{"9.10938356e-31kg"};
+    double mass_of_electron = std::stof(s5);
+    std::cout << mass_of_electron << "\n";
+
+    std::string s6{"0123a45"};
+    size_t pos = 0;
+    int i6     = std::stoi(s6, &pos, 10);
+    std::cout << "on " + s6 + " string, stoi stopped parsing at position " << pos
+              << " and resulted in " << i6 << "\n";
+
+    // Binary for 64000  and extra words that won't be processed
+    std::string s7 = "1111110001011000 is the postal code of my city.";
+    size_t position;
+    int i7 = std::stoi(s7, &position, 2);
+
+    std::string remainder = s7.substr(position);
+
+    std::cout << i7 << remainder << "\n";
+}
