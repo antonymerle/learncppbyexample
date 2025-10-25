ade1a565f0e3ff8ba938459444cf42c1fb86bede
Antony Merle <antony.merle@gmail.com>
2025-10-25 22:31:25 +0200

    add comments

--- destructors.cpp
+++ destructors.cpp
@@ -5,6 +5,9 @@
 class Talker
 {
    public:
+    // Prefixing the explicit keyword to the constructor prevents the compiler from using that
+    // constructor for implicit conversions.
+    // https://stackoverflow.com/questions/121162/what-does-the-explicit-keyword-mean
     explicit Talker(std::string name) : m_name{std::move(name)}
     {
         std::cout << m_name << " here\n";
@@ -26,4 +29,8 @@ void destructor()
         vec.emplace_back("Manon");
     }
     Talker basile("Basile");
+
+    // Manon signin off
+    // Basile signin off
+    // Antony signin off
 }
