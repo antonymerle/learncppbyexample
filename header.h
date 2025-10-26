eeb23ce135ea68bb9dc1d325163ddb800bf96be5
Antony Merle <antony.merle@gmail.com>
2025-10-26 21:58:02 +0100

    heap fn integration

--- header.h
+++ header.h
@@ -8,11 +8,18 @@ void constructor();
 void convert_string_to_number();
 void convert_number_to_string();
 void file_creation();
+void file_read();
+void file_read2();
 void guessing_game();
 void destructor();
 void exceptions();
 void hash_map_find();
 void unordered_set();
 void for_loop();
+void random_run();
+void random_gaussian_normal_dist();
+void range_views();
+void move_demo();
+void heap();
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,12 +10,19 @@ int main(void)
     convert_string_to_number();
     convert_number_to_string();
     file_creation();
-    destructor();
     exceptions();
     guessing_game();
     hash_map_find();
     unordered_set();
-#endif
     for_loop();
+    random_run();
+    random_gaussian_normal_dist();
+    range_views();
+    move_demo();
+    destructor();
+    file_read();
+    file_read2();
+#endif
+    heap();
     return 0;
 }
