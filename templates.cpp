5437d8cd7bd9a372906c210b13d59429a0fdaddf
Antony Merle <antony.merle@gmail.com>
2025-11-16 14:51:43 +0100

    templates 2

--- templates.cpp
+++ templates.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 
 template <typename T>
@@ -6,12 +7,35 @@ T cube(T t)
     return t * t * t;
 }
 
+template <typename T>
 struct Point {
-    int x, y, z;
+    T x, y, z;
 
     friend Point operator*(const Point& a, const Point& b)
     {
         return Point(a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+};
+
+template <typename T, size_t sz>
+class Array
+{
+   public:
+    T data_[sz];
+
+    T& operator[](size_t i)
+    {
+        return data_[i];
+    }
+
+    T* begin()
+    {
+        return data_;
+    }
+
+    T* end()
+    {
+        return data_ + sz;
     }
 };
 
@@ -24,9 +48,26 @@ void templates()
     ;  // type manually specified
 
     float pi{3.14159f};
-    std::cout << cube(pi);
+    std::cout << cube(pi) << "\n";
+    ;
 
     Point p{1, 2, 3};
     Point p2{cube(p)};
     std::cout << p2.x << ", " << p2.y << ", " << p2.z << "\n";
+
+    Point pf{1.33f, 2.33f, 3.33f};
+    Point pf2{cube(pf)};
+    std::cout << pf2.x << ", " << pf2.y << ", " << pf2.z << "\n";
+
+    Array<int, 3> array{1, 2, 3};
+
+    for (auto& it : array) {
+        cube(it);
+    }
+
+    for (auto i = array.begin(); i < array.end(); ++i) {
+        std::cout << *i << "\n";
+    }
+
+    std::cout << array.begin() << ", " << array[1] << ", " << array.end() << "\n";
 }
